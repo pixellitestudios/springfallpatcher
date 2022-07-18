e2f086988e84dd873d88b3a0b8b84649ce9f1394
Connor <baricaade@gmail.com>
2022-07-17 20:28:29 -0400

    Allow for certain colon commands to be whitelisted

--- src/main/java/studio/pixellite/patcher/patches/CommandColonSyntaxPatch.java
+++ src/main/java/studio/pixellite/patcher/patches/CommandColonSyntaxPatch.java
@@ -7,12 +7,22 @@ import me.lucko.helper.utils.Players;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class CommandColonSyntaxPatch implements TerminableModule {
+  /** A list of colon commands that will not be denied by this patch. */
+  private static final List<String> WHITELISTED_COMMANDS = List.of("towny:confirm",
+          "towny:deny");
+
   @Override
   public void setup(@NotNull TerminableConsumer consumer) {
     Events.subscribe(PlayerCommandPreprocessEvent.class)
             .handler(e -> {
               String[] command = e.getMessage().split(" ");
+
+              if(WHITELISTED_COMMANDS.contains(command[0])) {
+                return;
+              }
 
               if(command[0].contains(":") ||
                       !e.getPlayer().hasPermission("pixellite.bypasscolonsyntax")) {
